2718fc7c2fb023d91339a4f649334ab1fc8345b0
Claudio Scheer <claudioscheer@users.noreply.github.com>
2025-10-09 20:33:38 -0300

    feat: enhance PostScreen with scroll-to-top functionality and update InfiniteScroll to support scroll events

--- app/home/index.tsx
+++ app/home/index.tsx
@@ -1,7 +1,12 @@
 import { InfiniteScroll } from "@/components/InfiniteScroll";
 import { Post, PostsService } from "@/services/posts";
 import { useCallback, useEffect, useRef, useState } from "react";
-import { View } from "react-native";
+import {
+  FlatList,
+  NativeScrollEvent,
+  NativeSyntheticEvent,
+  View,
+} from "react-native";
 import { Card, Text } from "react-native-paper";
 
 const POSTS_LIMIT = 10;
@@ -10,6 +15,8 @@ export default function PostScreen() {
   const [data, setData] = useState<Post[]>([]);
   const [page, setPage] = useState(1);
   const [isLoading, setIsLoading] = useState(true);
+  const [showScrollTop, setShowScrollTop] = useState(false);
+  const flatListRef = useRef<FlatList>(null);
   const dataRef = useRef(data);
 
   const postService = new PostsService();
@@ -23,7 +30,10 @@ export default function PostScreen() {
     setIsLoading(true);
     try {
       const initialData = await postService.getPosts(POSTS_LIMIT, page);
-      console.log("initial data", initialData.posts.map(x => x.id))
+      console.log(
+        "initial data",
+        initialData.posts.map((x) => x.id),
+      );
       setData(initialData.posts);
     } catch (error) {
       console.log(error);
@@ -36,7 +46,10 @@ export default function PostScreen() {
     try {
       setPage(1);
       const initialData = await postService.getPosts(POSTS_LIMIT, 1);
-      console.log("refresh initial data", initialData.posts.map(x => x.id))
+      console.log(
+        "refresh initial data",
+        initialData.posts.map((x) => x.id),
+      );
       setData(initialData.posts);
     } catch (error) {
       console.log(error);
@@ -64,7 +77,7 @@ export default function PostScreen() {
     if (dataRef.current.length < 1) {
       return;
     }
-    
+
     setIsLoading(true);
     try {
       const newPage = page + 1;
@@ -77,6 +90,18 @@ export default function PostScreen() {
     setIsLoading(false);
   }, [page]);
 
+  const onScroll = useCallback(
+    async (event: NativeSyntheticEvent<NativeScrollEvent>) => {
+      const offsetY = event.nativeEvent.contentOffset.y;
+      setShowScrollTop(offsetY > 200);
+    },
+    [],
+  );
+
+  const scrollToTop = useCallback(() => {
+    flatListRef.current?.scrollToOffset({ offset: 0, animated: true });
+  }, []);
+
   useEffect(() => {
     loadInitialData();
   }, []);
@@ -84,12 +109,16 @@ export default function PostScreen() {
   return (
     <View>
       <InfiniteScroll
+        ref={flatListRef}
         data={data}
         renderItem={renderItem}
         keyExtractor={(item) => item.id.toString()}
         onLoadMore={handleLoadMore}
         onRefresh={onRefresh}
         isLoading={isLoading}
+        onScroll={onScroll}
+        showScrollToTop={showScrollTop}
+        onScrollToTop={scrollToTop}
       />
     </View>
   );
--- components/InfiniteScroll.tsx
+++ components/InfiniteScroll.tsx
@@ -1,41 +1,80 @@
-import { FlatList, ListRenderItem, Text } from "react-native";
+import { Ref } from "react";
+import {
+  FlatList,
+  ListRenderItem,
+  NativeScrollEvent,
+  NativeSyntheticEvent,
+  StyleSheet,
+  Text
+} from "react-native";
+import { FAB } from "react-native-paper";
 
 export type InfiniteScrollProps<T> = {
+  ref: Ref<FlatList>;
   data: T[];
   renderItem: ListRenderItem<T>;
   keyExtractor: (item: T, index: number) => string;
   onLoadMore: () => Promise<void>;
   onRefresh: () => Promise<void>;
   isLoading: boolean;
+  onScroll: (event: NativeSyntheticEvent<NativeScrollEvent>) => Promise<void>;
+  showScrollToTop: boolean;
+  onScrollToTop: () => void;
 };
 
 export function InfiniteScroll<T>(props: InfiniteScrollProps<T>) {
   const {
     data,
-    renderItem, keyExtractor, onLoadMore,
+    renderItem,
+    keyExtractor,
+    onLoadMore,
     isLoading,
     onRefresh,
+    ref,
+    onScroll,
+    showScrollToTop,
+    onScrollToTop
   } = props;
 
   async function handleEndReached() {
     if (data.length < 1) {
-        return;
+      return;
     }
 
-    console.log("handle load more")
+    console.log("handle load more");
     await onLoadMore();
   }
 
   return (
-    <FlatList
-      data={data}
-      renderItem={renderItem}
-      keyExtractor={keyExtractor}
-      onEndReachedThreshold={0.3}
-      onEndReached={handleEndReached}
-      ListEmptyComponent={isLoading ? null : <Text>Empty List</Text>}
-      refreshing={isLoading}
-      onRefresh={onRefresh}
-    />
+    <>
+      <FlatList
+        ref={ref}
+        data={data}
+        renderItem={renderItem}
+        keyExtractor={keyExtractor}
+        onEndReachedThreshold={0.3}
+        onEndReached={handleEndReached}
+        ListEmptyComponent={isLoading ? null : <Text>Empty List</Text>}
+        refreshing={isLoading}
+        onRefresh={onRefresh}
+        onScroll={onScroll}
+      />
+      {showScrollToTop &&
+        <FAB
+          icon="arrow-up"
+          style={styles.fab}
+          onPress={() => onScrollToTop()}
+        />
+      }
+    </>
   );
 }
+
+const styles = StyleSheet.create({
+  fab: {
+    position: "absolute",
+    margin: 16,
+    right: 0,
+    bottom: 0,
+  },
+});
